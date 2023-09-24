67120cc1ba34a67082963220b1193486ce9f2bf5
Jonathan Lundholm <jon.lundholm@gmail.com>
2023-09-24 23:11:30 +0200

    chore: remove Doc.String() method

--- doc.go
+++ doc.go
@@ -1,23 +1,9 @@
 package gokdl
 
-import (
-	"strings"
-)
-
 type Doc struct {
 	nodes []Node
 }
 
 func (d Doc) Nodes() []Node {
 	return d.nodes
-}
-
-func (d Doc) String() string {
-	nodes := []string{}
-	for _, node := range d.nodes {
-		nsdfnsd := stringRecNode("", node)
-		nodes = append(nodes, nsdfnsd)
-	}
-
-	return strings.Join(nodes, "\n")
 }
--- node.go
+++ node.go
@@ -1,54 +1,8 @@
 package gokdl
 
-import (
-	"fmt"
-	"strings"
-)
-
 type Node struct {
 	Name     string
 	Children []Node
 	Props    []Prop
 	Args     []Arg
-}
-
-func stringRecNode(indentation string, n Node) string {
-	double := indentation + "  "
-
-	builder := strings.Builder{}
-	args := []string{}
-	for _, arg := range n.Args {
-		args = append(args, fmt.Sprint(arg))
-	}
-
-	props := []string{}
-	for _, prop := range n.Props {
-		props = append(props, fmt.Sprintf("%s=%v", prop.Name, prop.Value))
-	}
-
-	builder.WriteString(indentation + n.Name)
-
-	if len(args) > 0 {
-		builder.WriteString("\n")
-		builder.WriteString(double + "Args:  " + strings.Join(args, ", "))
-
-	}
-
-	if len(props) > 0 {
-		builder.WriteString("\n")
-		builder.WriteString(double + "Props: " + strings.Join(props, ", "))
-	}
-
-	if len(n.Children) > 0 {
-		builder.WriteString("\n")
-		builder.WriteString(double + "Children\n")
-	}
-
-	for _, node := range n.Children {
-		sss := stringRecNode(double, node)
-		builder.WriteString(sss)
-		builder.WriteString("\n")
-	}
-
-	return builder.String()
 }
